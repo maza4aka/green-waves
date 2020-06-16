3a9a4579fb461d601dd41f64beb8574886ab7703
Asya <asya.kosareva666@gmail.com>
2020-06-16 20:55:31 +0300

    Add speed randomizing

--- route.py
+++ route.py
@@ -3,10 +3,14 @@
 
 nodes = []
 
+function randomInt(min, max) {
+	return min + Math.floor((max - min) * Math.random());
+}
+
 with open("route.txt", 'r') as routes:
     for route in routes:
         lat, lon = route.strip().split(', ')
-        nodes.append(f'{{"id":"ambulance69","lat":{lat},"lon":{lon},"spd":69}};\n')
+        nodes.append(f'{{"id":"ambulance69","lat":{lat},"lon":{lon},"spd":{randomInt(50, 80)}}};\n')
 
 with open("route.json", 'w') as json:
     json.writelines(nodes)
