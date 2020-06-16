93809e226e62fa26a0c0a3541a19e05b4e4902d0
Asya <asya.kosareva666@gmail.com>
2020-06-16 21:30:14 +0300

    Fix typo

--- route.py
+++ route.py
@@ -1,16 +1,13 @@
 #!/usr/local/bin/python3.7m
 
+from random import randint 
 
 nodes = []
-
-function randomInt(min, max) {
-	return min + Math.floor((max - min) * Math.random());
-}
 
 with open("route.txt", 'r') as routes:
     for route in routes:
         lat, lon = route.strip().split(', ')
-        nodes.append(f'{{"id":"ambulance69","lat":{lat},"lon":{lon},"spd":{randomInt(50, 80)}}};\n')
+        nodes.append(f'{{"id":"ambulance69","lat":{lat},"lon":{lon},"spd":{randint(50, 80)}}};\n')
 
 with open("route.json", 'w') as json:
     json.writelines(nodes)
